2cfa4841b9aba8034763ac15f63dbd7e94cddd3c
samavati <samavati.ehsan@yahoo.com>
2020-08-12 20:54:51 -0400

    comment edited

--- src/app/ticket-comments/ticket-comments.component.ts
+++ src/app/ticket-comments/ticket-comments.component.ts
@@ -1,22 +1,37 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, OnInit, Inject, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogContent } from '@angular/material/dialog';
 import { Ticket } from '../shared/models/ticket.model';
 import { TicketService } from '../shared/ticket.service';
 import { FormControl } from '@angular/forms';
+import { Comment } from '../shared/models/comment.model';
+import * as moment from 'jalali-moment';
 
 @Component({
   selector: 'app-ticket-comments',
   templateUrl: './ticket-comments.component.html',
   styleUrls: ['./ticket-comments.component.scss']
 })
-export class TicketCommentsComponent {
+export class TicketCommentsComponent implements OnInit, AfterViewInit {
 
   comment = new FormControl();
+  comments: Comment[] = [];
+  newComments: Comment[] = [];
+
+  @ViewChild('dialogBody', { static: true }) dialogBody: ElementRef<HTMLDivElement>;
 
   constructor(
     public dialogRef: MatDialogRef<TicketCommentsComponent>,
     public ticketService: TicketService,
     @Inject(MAT_DIALOG_DATA) public data: { ticket: Ticket }) { }
+
+  ngOnInit(): void {
+    this.comments = [...this.data.ticket.comments];
+    this.comments.reverse();
+  }
+
+  ngAfterViewInit(): void {
+    this.dialogBody.nativeElement.scrollTop = this.dialogBody.nativeElement.scrollHeight;
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
@@ -28,8 +43,21 @@ export class TicketCommentsComponent {
   }
 
   onSend(): void {
-    this.ticketService.creatComment(this.data.ticket.ID, 'ehsan@yahoo.com', this.comment.value)
-      .subscribe(res => { });
+    // this.ticketService.creatComment(this.data.ticket.ID, 'ehsan@jibit.com', this.comment.value)
+    //   .subscribe(res => { });
+    this.newComments.push({
+      ticketID: this.data.ticket.ID,
+      content: this.comment.value as string,
+      owner: '',
+      createdAt: moment().format(),
+      metadata: '',
+      modifiedAt: '',
+    });
+    setTimeout(() => {
+      this.dialogBody.nativeElement.scrollTop = this.dialogBody.nativeElement.scrollHeight;
+    }, 0);
+
+    this.comment.reset();
   }
 
 }
